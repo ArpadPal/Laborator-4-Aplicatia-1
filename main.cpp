4fa65cb9daf49e705e8b19c203c1b6a81330e654
Pal Arpad <arpika@gmail.com>
2020-09-12 07:10:07 +0000

    Laborator 4 aplicația 3

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int main() {
-  int a[50], n, i;
-  cout << "Numarul elementelot din sir: "; cin >> n;
-  for(i=0; i<n; i++){
-    cout << "a[" << i << "]= "; cin >> a[i];
+  int a[20], nr, i, j, aux;
+  cout << "Numarul de elemente din sirul a (maximum 20): "; 
+  cin >> nr;
+  for (i = 0; i < nr; i++) {
+    cout << "a[" << i << "] = "; 
+    cin >> a[i];
   }
-  int max;
-   max = a[0];
-   for (i = 0; i < n; i++)
-   {
-      if(max < a[i])
-        max = a[i];
+  for(i=0, j=nr-1; i<j; i++, j--)
+  {
+    aux = a[i];
+    a[i] = a[j];
+    a[j] = aux;
+  }
+  for (i = 0; i < nr; i++) {
+      cout << a[i] << " ";
    }
-   cout << "Valoarea maxima este : " << max << endl;
+   cout << endl;
 }
